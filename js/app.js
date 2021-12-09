e1cc342b7fef56b43568fdbf13c721a2659b65ab
fercovalle <carloscoronado201932190@cunoc.edu.gt>
2021-12-09 08:54:53 -0600

    agregando funcion getDatosRegisterUser y probando su funcionalidad

--- js/app.js
+++ js/app.js
@@ -12,3 +12,24 @@ function getDatosLogin(){
     const password = document.getElementById("password-login").value;
     console.log("email: "+email+" password: " +password);*/
 }
+function getDatosRegisterUser(){
+    const userRegister={
+        emailRegister: "fernandocovalle@gmail.com",
+        nameRegister : "Fernando",
+        lastNameRegister: "Coronado",
+        passwordRegister : "fer123",
+        passwordRegisterConfirm : "fer123",
+    }
+    userRegister.emailRegister=document.getElementById("input-email").value;
+    userRegister.nameRegister=document.getElementById("input-name").value;
+    userRegister.lastNameRegister=document.getElementById("input-last-name").value;
+    userRegister.passwordRegister=document.getElementById("input-password").value;
+    userRegister.passwordRegisterConfirm=document.getElementById("input-password-confirm").value;
+
+    if(userRegister.passwordRegister === userRegister.passwordRegisterConfirm){
+        console.log("Datos Registrados");
+        console.log(userRegister);
+    }else{
+        console.log("Las contraseñas no coinciden");
+    }
+}
